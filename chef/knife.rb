5dd965b9fda5678e108ee5831cf1d62f075507d8
Kai Forsthövel <kai.forsthoevel@invision.de>
2014-06-04 12:58:52 +0200

    Set default chef-vault mode to 'client'

--- chef/knife.rb
+++ chef/knife.rb
@@ -18,3 +18,5 @@ knife[:aws_ssh_key_id]        = ENV['AWS_SSH_KEY']
 knife[:aws_access_key_id]     = ENV['AWS_ACCESS_KEY_ID']
 knife[:aws_secret_access_key] = ENV['AWS_SECRET_ACCESS_KEY']
 knife[:region]                = ENV['AWS_DEFAULT_REGION']
+
+knife[:vault_mode] = 'client'
