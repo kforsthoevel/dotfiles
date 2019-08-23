5143de155c155ba8efe8c39e31d714589ab55a1c
Kai Forsthövel <kai.forsthoevel@invision.de>
2019-08-23 10:23:37 +0200

    Add alias for tmuxinator

--- aliases.zsh
+++ aliases.zsh
@@ -29,6 +29,7 @@ alias la='ls -lAh'
 alias ll='ls -lh'
 alias localip="ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'"
 alias ls='ls -G'
+alias mux="tmuxinator"
 alias path='echo $PATH | tr -s ":" "\n"'
 alias pubip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias tf="terraform"
