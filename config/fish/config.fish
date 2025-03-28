8cf90e37221dc36e62c7b2b73c14d6273c6925e3
Kai Forsthövel <kai.forsthoevel@peopleware.com>
2025-03-28 16:32:11 +0100

    Add config and aliases for fish

---
+++ config/fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    source ~/.config/fish/aliases
+    if test -r ~/.config/fish/aliases.local
+        source ~/.config/fish/aliases.local
+    end
+
+    atuin init fish --disable-up-arrow | source
+
+end
+
+starship init fish | source
+mise activate fish | source
+zoxide init fish | source
